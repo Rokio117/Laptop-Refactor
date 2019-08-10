e853db09422aded10524428d7885a49494f463c3
Nick Hillary <hillan02@gmail.com>
2019-08-10 13:26:14 -0500

    made separate class folders

---
+++ src/NewComponents/summaryOption.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+ export default class SummaryOption extends Component {
+  render() {
+    return(
+      <div className="summary__option" key={featureHash}>
+          <div className="summary__option__label">{feature} </div>
+          <div className="summary__option__value">{selectedOption.name}</div>
+          <div className="summary__option__cost">
+            {USCurrencyFormat.format(selectedOption.cost)}
+          </div>
+        </div>
+    )
+  }
+}
